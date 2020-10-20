390d9338093e56cedd593d523b70c202a5997a0f
Carlos Alexandre <63723135+RaikanBrK@users.noreply.github.com>
2020-10-20 14:59:43 -0300

    Criar Header
    Criando menu de navegação

---
+++ countries/views/header.php
@@ -0,0 +1,25 @@
+<header>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+
+		<a href="/index.php" class="navbar-brand logo-text">Willian</a>
+
+		<button class="navbar-toggler" id="hamburguer" type="button" data-toggle="collapse" data-target="#menu" aria-label="Toggle navigation">
+			<i class="fas fa-bars"></i>
+		</button>
+
+		<div class="collapse navbar-collapse" id="menu">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="/index.php">Search for Countries</a>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="/countries.php">Countries</a>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="/student.php">Student</a>
+				</li>
+			</ul>
+		</div>
+
+	</nav>
+</header>
