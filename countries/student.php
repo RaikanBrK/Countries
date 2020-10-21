8c885e227abdf804ab28b9ef8c0fb4bea832b721
Carlos Alexandre <63723135+RaikanBrK@users.noreply.github.com>
2020-10-21 08:52:56 -0300

    Atualizando head do student.php
    Atualizando scripts e links do head

--- countries/student.php
+++ countries/student.php
@@ -21,10 +21,14 @@
 	<link rel="stylesheet" href="css/footer.css">
 	<link rel="stylesheet" href="css/media-query.css">
 	
-	<link rel="stylesheet" href="css/search-countries.css">
+	<link rel="stylesheet" href="css/student.css">
+
+	<link rel="stylesheet" href="css/media-query.css">
+
+	<!-- Jquery -->
+	<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 
 	<!-- Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </head>
@@ -38,7 +42,9 @@
 	?>
 
 	<div class="content">
-		
+		<main>
+			
+		</main>
 	</div>
 
 	<?php 
@@ -46,6 +52,8 @@
 			require_once('views/footer.php');
 		}
 	?>
+
+	<script type="text/javascript" src="js/script.js"></script>
 
 </body>
 </html>
